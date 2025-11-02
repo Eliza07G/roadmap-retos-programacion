8eea522eb049d99693f6633a952e2efdcec2a210
Eliza07G <emggposch.2000@gmail.com>
2025-11-01 22:19:04 -0400

    Create Eliza07G.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Eliza07G.py
@@ -0,0 +1,16 @@
+#comentario en https://www.python.org/
+#yo los uso para documentar todo lo que hago en un ejercico
+
+#variables
+var_one=2000
+var_two=1000
+print(f"{variable},{var}")
+
+#datos
+int(30)
+flout(12.6)
+str(" cadenas de texto")
+bool(True False )
+
+
+print("Hola mundo :) ")
